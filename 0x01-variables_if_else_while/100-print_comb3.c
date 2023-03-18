13bb1322dd3e0110438127d20da9709227ac51d1
karim hashim <karimhashim27@gmail.com>
2023-03-17 20:06:09 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@ int main(void)
 {
 	int i, j;
 
-	for(i = 48 ; i < 57 ; i++)
+	for (i = 48 ; i < 57 ; i++)
 	{
 		for (j = i + 1; j <= 57; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if( i != 56)
+			if (i != 56)
 			{
 				putchar(',');
 				putchar(' ');
