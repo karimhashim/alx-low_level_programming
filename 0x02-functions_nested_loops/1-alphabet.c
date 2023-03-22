e4afb3b21d6d94c749edf3b6a46cf284dc04019e
karim hashim <karimhashim27@gmail.com>
2023-03-22 16:11:21 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,15 @@
 /**
 * print_alphabet - print all alphabet in lowercase
 */
+
 void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-		_putchar('\n');
+
+	_putchar('\n');
 }
 
 
