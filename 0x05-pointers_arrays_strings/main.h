b11aa428289b56392fcfdc20933411bf307fa055
karim hashim <karimhashim27@gmail.com>
2023-03-27 17:27:28 -0700

    last

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef main_h
 #define main_h
 
+
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
