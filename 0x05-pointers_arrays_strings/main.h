5908a7439f5adef81328bd438732f1148182ddaf
karim hashim <karimhashim27@gmail.com>
2023-03-27 17:07:49 -0700

    swap

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef main_h
 #define main_h
 
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
 void reset_to_98(int *n);
 void more_numbers(void);
 void print_line(int n);
