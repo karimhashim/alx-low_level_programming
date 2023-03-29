52c6cec0c624d20eaca903044e2bdd6fb8b14f53
karim hashim <karimhashim27@gmail.com>
2023-03-29 16:48:30 -0700

    more pointer

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef main_h
+#define main_h
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+
+#endif
+
