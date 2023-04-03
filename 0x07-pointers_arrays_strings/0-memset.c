1854c54d80f2eb45fa8e62f6ce00ea87c805303c
karim hashim <karimhashim27@gmail.com>
2023-04-03 12:14:13 -0700

    1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,13 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
-		{
-			s[i] = b;
-		}
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
 	return (s);
+
 }
