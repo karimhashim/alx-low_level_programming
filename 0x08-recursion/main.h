e9a5937a9606417c7faafdbf1337b1b610e15465
karim hashim <karimhashim27@gmail.com>
2023-04-05 16:55:36 -0700

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,36 @@
+#ifndef main_h
+#define main_h
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_numbers(void);
+void print_most_numbers(void);
+int mul(int a, int b);
+int _isupper(int c);
+int _isdigit(int c);
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+
+#endif
+
