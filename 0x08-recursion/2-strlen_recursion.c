8a6114466e72ded2d74b8a3bf018b8cf79359929
karim hashim <karimhashim27@gmail.com>
2023-04-05 17:18:29 -0700

    recur

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _srtlen_recursion(s + 1));
+	} else
+	{
+		return (0);
+	}
+}
