41c6e53ce7f2cb976806cde824afeb0a8b981937
karim hashim <karimhashim27@gmail.com>
2023-04-09 07:33:41 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -45,6 +45,13 @@ int _atoi(char *s)
 
 	return (n);
 }
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
 
 int main(int argc, char *argv[])
 {
