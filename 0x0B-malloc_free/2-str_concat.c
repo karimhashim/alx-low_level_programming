3ae40e9bb411113f05bf26c44f5850eb1448e22f
karim hashim <karimhashim27@gmail.com>
2023-04-11 03:37:50 -0700

    f

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,8 +18,10 @@ char *str_concat(char *s1, char *s2)
 		i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
+
 	while (s2[ci] != '\0')
 		ci++;
+
 	conct = malloc(sizeof(char) * (i + ci + 1));
 	if (conct == NULL)
 		return (NULL);
